b88ca39e943a66175f8f41dacd13ead5b7eb8435
Jaskaran Singh <jaskaran1523@gmail.com>
2021-12-04 20:06:18 -0800

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// headers: {
+//     'x-rapidapi-host': 'bayut.p.rapidapi.com',
+//     'x-rapidapi-key': '7903765e48mshf2cee88f72535f2p15d9f6jsn7f156066cfe3'
+//   }
+
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+export const fetchApi = async (url) => {
+	const { data }= await axios.get((url), {
+		headers: {
+			'x-rapidapi-host': 'bayut.p.rapidapi.com',
+			'x-rapidapi-key': '7903765e48mshf2cee88f72535f2p15d9f6jsn7f156066cfe3'
+		}
+	});
+
+	return data;
+}
